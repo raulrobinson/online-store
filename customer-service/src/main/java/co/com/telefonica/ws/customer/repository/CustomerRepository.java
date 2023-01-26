b6502f6d1b00e91858fb4d758fd75097105b63cd
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-01-26 15:14:49 -0500

    All

---
+++ customer-service/src/main/java/co/com/telefonica/ws/customer/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package co.com.telefonica.ws.customer.repository;
+
+import co.com.telefonica.ws.customer.entity.Customer;
+
+import co.com.telefonica.ws.customer.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository  extends JpaRepository<Customer,Long> {
+        public Customer findByNumberID(String numberID);
+        public List<Customer> findByLastName(String lastName);
+        public List<Customer> findByRegion(Region region);
+}
